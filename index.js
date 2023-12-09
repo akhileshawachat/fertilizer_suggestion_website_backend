7cc0592a86f942dd04af49f4edd614867cfbc898
akhileshawachat <akhileshawachat65@gmail.com>
2023-12-09 20:25:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import express  from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+dotenv.config();
+import connectToMongo from "./db.js";
+import authRoute from "./routes/authRoute.js";
+
+
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
+
+connectToMongo();
+
+
+let port = process.env.PORT;
+
+app.get("/",(req,res)=>{
+    res.status(200).send("Hi from server");
+});
+
+app.get("/connect-to-server",(req,res)=>{
+    res.status(200).send("Connected to server");
+});
+
+app.use('/auth',authRoute);
+
+app.listen(port,()=>console.log(`Server listening at port ${port}`));
